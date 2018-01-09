4ee0847a8aec4dcfc41599648e5766d152479952
PeckLiu2017 <xiaoyang20170204@gmail.com>
2018-01-09 16:11:35 +0800

    angular-demo

--- backbone-demo/js/main.js
+++ backbone-demo/js/main.js
@@ -7,6 +7,11 @@ window.App = {
 	Router: {}
 };
 
+// Backbone 只有 M 和 V,没有 C.因为,前端 Controller 与后端不同。
+// 不需要,也不应该处理业务逻辑
+// 只需要处理 UI 逻辑,响应用户的一举一动
+// Backbone 没有 C，只用事件来处理 UI 逻辑
+
 App.Router = Backbone.Router.extend({
 	routes: {
 		'': 'index',
@@ -34,7 +39,7 @@ App.Router = Backbone.Router.extend({
 
 	default: function(other) {
 		$(document.body).append("你访问的 " + other + " 路由未定义<br>");
-		
+
 	}
 
 });
