18f9039d82f143acf3b79d373e56e4943181ad0b
PeckLiu2017 <xiaoyang20170204@gmail.com>
2018-01-11 17:52:42 +0800

    defaultsAndDestructuring

--- ES6syntax/restParameter.js
+++ ES6syntax/restParameter.js
@@ -16,3 +16,20 @@ function sum(...nums) {
 }
 
 console.log(sum(10, 36)); // 46
+
+// get average
+function average(...nums) {
+  if(nums.length === 0){
+    return 0;
+  }
+  let total = 0;
+  for(const num of nums) {
+    total += num;
+  }
+  return total/nums.length;
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
