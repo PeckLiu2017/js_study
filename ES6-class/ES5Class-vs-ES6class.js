21d9b7de088c272c49579e2fb56e7db6ee04528a
PeckLiu2017 <xiaoyang20170204@gmail.com>
2018-01-11 20:11:54 +0800

    ES5Class-vs-ES6class

---
+++ ES6-class/ES5Class-vs-ES6class.js
@@ -0,0 +1,30 @@
+// ES5
+function Plane(numEngines) {
+  this.numEngines = numEngines;
+  this.enginesActive = false;
+}
+
+// methods "inherited" by all instances
+Plane.prototype.startEngines = function () {
+  console.log('starting engines...');
+  this.enginesActive = true;
+};
+
+const richardsPlane = new Plane(1);
+richardsPlane.startEngines();
+
+const jamesPlane = new Plane(4);
+jamesPlane.startEngines();
+
+// ES6
+class Plane {
+  constructor(numEngines) {
+    this.numEngines = numEngines;
+    this.enginesActive = false;
+  }
+
+  startEngines() {
+    console.log('starting engines…');
+    this.enginesActive = true;
+  }
+}
